be7f5bcb7352113fede52a638ae4f6e0590ed62d
Prabukhanth T <prabu@criodo.com>
2024-09-19 00:07:25 +0530

    stopwatch

---
+++ xstopwatch/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  const handleStartStop = () => {
+    setIsRunning((prev) => !prev);
+  }
+
+  useEffect(() => {
+    let id;
+    if(isRunning){
+        id = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+    return () => clearInterval(id)
+  }, [isRunning]);
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  }
+  
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(0);
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Stopwatch</h1>
+      </div>
+      <div>
+        <p>Time: {formatTime(time)}</p>
+        <button className='startButton' onClick={handleStartStop}>{isRunning ? "Stop" : "Start"}</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
